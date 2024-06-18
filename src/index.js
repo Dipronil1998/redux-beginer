d819472ab366d4d20e0062ff54b8686a4cddc260
dipronil <dipronil.das@techwens.in>
2024-06-18 22:07:00 +0530

    auth

--- src/index.js
+++ src/index.js
@@ -4,26 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { extendedApiSlice } from './features/posts/PostsSlice';
-import { usersApiSlice } from './features/users/UsersSlice';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
-store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
